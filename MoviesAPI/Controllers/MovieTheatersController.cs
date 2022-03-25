4dcdd78f8eb7de74664c0da894095f6b7667104f
Bruno <pj_antoniobruno@bocombbm.com.br>
2022-03-25 16:37:37 -0300

    Seção - Edit Movie Theater - 25031637

---
+++ MoviesAPI/Controllers/MovieTheatersController.cs
@@ -0,0 +1,84 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MoviesAPI.DTOs;
+using MoviesAPI.Entities;
+using MoviesAPI.Helpers;
+
+namespace MoviesAPI.Controllers
+{
+    [ApiController]
+    [Route("api/movietheaters")]
+    public class MovieTheatersController: ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public MovieTheatersController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<MovieTheaterDTO>>> Get([FromQuery] PaginationDTO paginationDTO)
+        {
+            var queryable = _context.MovieTheaters.AsQueryable();
+            await HttpContext.InsertParametersPaginationInHeader(queryable);
+            var entities = await queryable.OrderBy(x => x.Name).Paginate(paginationDTO).ToListAsync();
+            return _mapper.Map<List<MovieTheaterDTO>>(entities);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<MovieTheaterDTO>> Get(int id)
+        {
+            var movieTheater = await _context.MovieTheaters.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (movieTheater == null)
+            {
+                return NotFound();
+            }
+
+            return _mapper.Map<MovieTheaterDTO>(movieTheater);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(MovieTheaterCreationDTO movieCreationDTO)
+        {
+            var movieTheater = _mapper.Map<MovieTheater>(movieCreationDTO);
+            _context.Add(movieTheater);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(int id, MovieTheaterCreationDTO movieCreationDTO)
+        {
+            var movieTheater = await _context.MovieTheaters.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (movieTheater == null)
+            {
+                return NotFound();
+            }
+
+            movieTheater = _mapper.Map(movieCreationDTO, movieTheater);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var movieTheater = await _context.MovieTheaters.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (movieTheater == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(movieTheater);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
